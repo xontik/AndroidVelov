2b511d47cb07a946d6242ddde86a742cecf764b5
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2018-04-04 22:25:54 +0200

    Ajout autocomplete

--- app/src/main/java/fr/iutlyon1/androidvelov/MainActivity.java
+++ app/src/main/java/fr/iutlyon1/androidvelov/MainActivity.java
@@ -1,14 +1,18 @@
 package fr.iutlyon1.androidvelov;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +23,7 @@ import android.view.MenuItem;
 import android.widget.EditText;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import fr.iutlyon1.androidvelov.model.VelovData;
@@ -28,8 +33,8 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
             StationListFragment.OnListFragmentInteractionListener,
             StationMapFragment.OnMapFragmentInteractionListener {
-
     private DrawerLayout drawer;
+    private SearchView mSearchView;
 
     private Fragment fragmentMap = null;
     private Fragment fragmentList = null;
@@ -81,12 +86,20 @@ public class MainActivity extends AppCompatActivity
         final MenuItem searchItem = menu.findItem(R.id.action_search);
 
         if (searchItem != null) {
-            SearchView searchView = (SearchView) searchItem.getActionView();
+            mSearchView = (SearchView) searchItem.getActionView();
+            mSearchView.setSuggestionsAdapter(new SimpleCursorAdapter(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    generateCursor(mDataset.getStations()),
+                    new String[] { SearchManager.SUGGEST_COLUMN_TEXT_1 },
+                    new int[] { android.R.id.text1 },
+                    0
+            ));
 
-            EditText searchPlate = searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+            EditText searchPlate = mSearchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
             searchPlate.setHint(R.string.station_map_search_hint);
 
-            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
                 @Override
                 public boolean onQueryTextSubmit(String query) {
                     return false;
@@ -98,14 +111,54 @@ public class MainActivity extends AppCompatActivity
                     return false;
                 }
             });
+
+            mSearchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+                @Override
+                public boolean onSuggestionSelect(int position) {
+                    Cursor cursor = (Cursor) mSearchView.getSuggestionsAdapter().getItem(position);
+                    String fullname = cursor.getString(cursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_1));
+                    cursor.close();
+
+                    searchPlate.setText(fullname);
+                    return true;
+                }
+
+                @Override
+                public boolean onSuggestionClick(int position) {
+                    return onSuggestionSelect(position);
+                }
+            });
+
+
+            mDataset.addOnFilterUpdateListener((filter, filteredStations, dataset) -> {
+                Cursor cursor = generateCursor(filteredStations);
+
+                mSearchView.getSuggestionsAdapter().changeCursor(cursor);
+            });
         }
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private Cursor generateCursor(List<VelovStationData> stations) {
+        MatrixCursor cursor = new MatrixCursor(
+                new String[] { "_id", "suggest_text_1" }
+        );
+
+        for (VelovStationData station : stations) {
+            cursor.newRow()
+                    .add("_id", station.getNumber())
+                    .add("suggest_text_1", station.getFullName());
+        }
+
+        return cursor;
     }
 
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else if (!mSearchView.isIconified()) {
+            mSearchView.setIconified(true);
         } else {
             super.onBackPressed();
         }
